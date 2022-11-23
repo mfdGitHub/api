e7ddd5adbb7fbdfb9b819c3743b2b550dc429c70
MoisesDebian9 <moyinformatico@gmail.com>
2022-11-22 20:24:43 -0600

    Se agregaron los nombre de funciones que se van a programar

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
+
+def saveCustomer():
+    return "ok"
+
+def removeCustomer():
+    return "ok"
+
+def getCustomer():
+    return "ok"
+
+def getAllCustomer():
+    return "ok"
+
+
 @app.route('/')
-def Index():
+def index():
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(None, 3000, True)
