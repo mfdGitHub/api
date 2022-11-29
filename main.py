b30640506140f8982fc46f0ce8aaf698da279810
MoisesDebian9 <moyinformatico@gmail.com>
2022-11-28 18:51:43 -0600

    Guardar cambios antes de merge

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 from flask import Flask, render_template
-
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.config['MySQL_HOST'] = 'localhost'
+app.config['MySQL_USER'] = 'root'
+app.config['MySQL_PASSWORD'] = 'Sup3rR00t.'
+app.config['MySQL_DB'] = 'system'
+mysql = MySQL(app)
+
+@app.route('/customers') # GET
+def getAllCustomers():
+    return "Devuelve listado de clientes"
 
 @app.route('/customers/<int:id>') # GET
 def getCustomer(id):
-    return "ok"
-@app.route('/customers') # GET
-def getAllCustomers():
-    return "ok"
+    return "Devualve un cliente"
 
 @app.route('/customers', methods=['POST']) # POST
 def saveCustomer():
-    return "ok"
-@app.route('/customers', methods=['DELETE'])
-def removeCustomer():
-    return "ok"
+    return "Cliente guardado"
+@app.route('/customers/<int:id>', methods=['DELETE'])
+def removeCustomer(id):
+    return "Cliente eliminado"
 
 
 @app.route('/')
